7c22cdc3243aaaf7a8c0b116f44e29656a24e8ac
JGaviria0 <j.gaviria@utp.edu.co>
2023-03-21 22:57:55 -0500

    Add modal and correct some typos

--- pages/form.js
+++ pages/form.js
@@ -87,7 +87,7 @@ const MainForm = () => {
       }),
   });
   const [snackbarStatus, setSnackbarStatus] = useState(false);
-
+  const [showModal, setShowModal] = useState(false);
   const [selectedFile, setSelectedFile] = useState("No seleccionado");
   const [fileUrl, setFileUrl] = useState(null);
 
@@ -142,6 +142,8 @@ const MainForm = () => {
               href=""
               sx={{
                 mr: 2,
+                minHeight: 30,
+                minWidth: 100,
                 display: "flex",
                 flexGrow: 1,
                 fontFamily: "monospace",
@@ -150,7 +152,7 @@ const MainForm = () => {
                 textDecoration: "none",
               }}
             >
-              Acción grupo biopolimeros
+              Acción Biopolimeros
             </Typography>
 
             <Box sx={{ flexGrow: 0 }}>
@@ -183,7 +185,7 @@ const MainForm = () => {
                 <MenuItem onClick={() => firebase.auth().signOut()}>
                   <Typography textAlign="center">Cerrar sesión</Typography>
                 </MenuItem>
-              </Menu>
+              </Menu> 
             </Box>
           </Toolbar>
         </Container>
@@ -374,12 +376,14 @@ const MainForm = () => {
             {errors.application_file?.message}
           </Typography>
         </Box>
-        <Button variant="contained" onClick={handleSubmit(onSubmit)}>
+        <button 
+          className="my-5 w-auto px-8 h-10 bg-blue-600 text-white rounded-md shadow hover:shadow-lg font-semibold" 
+          onClick={handleSubmit(onSubmit)}>
           Enviar
-        </Button>
+        </button>
         <Typography variant="p">
-          Al dar click en &apos;Enviar&apos; está aceptando nuestros términos y
-          condiciones.
+          Al dar click en &apos;Enviar&apos; está aceptando nuestros 
+          <a className="font-medium text-blue-600 underline dark:text-blue-500 hover:no-underline" onClick={() => setShowModal(true)}> términos y condiciones </a>.
         </Typography>
         <NotificationModal
           isOpen={snackbarStatus}
@@ -388,6 +392,79 @@ const MainForm = () => {
           }}
         />
       </Paper>
+      {showModal? (
+        <>
+        <div
+          className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+        >
+          <div className="relative w-auto my-6 mx-auto max-w-3xl">
+            {/*content*/}
+            <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+              {/*header*/}
+              <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                <h3 className="text-3xl text-slate-500">
+                  Términos y condiciones
+                </h3>
+                <button
+                  className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                  onClick={() => setShowModal(false)}
+                >
+                  <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                    ×
+                  </span>
+                </button>
+              </div>
+              {/*body*/}
+              <div className="relative p-6 flex-auto">
+                <p className="my-4 text-slate-500 text-lg leading-relaxed">
+                  I always felt like I could do anything. That’s the main
+                  thing people are controlled by! Thoughts- their perception
+                  of themselves! They're slowed down by their perception of
+                  themselves. If you're taught you can’t do anything, you
+                  won’t do anything. I was taught I could do everything.
+                  I always felt like I could do anything. That’s the main
+                  thing people are controlled by! Thoughts- their perception
+                  of themselves! They're slowed down by their perception of
+                  themselves. If you're taught you can’t do anything, you
+                  won’t do anything. I was taught I could do everything.
+                  I always felt like I could do anything. That’s the main
+                  thing people are controlled by! Thoughts- their perception
+                  of themselves! They're slowed down by their perception of
+                  themselves. If you're taught you can’t do anything, you
+                  won’t do anything. I was taught I could do everything.
+                  I always felt like I could do anything. That’s the main
+                  thing people are controlled by! Thoughts- their perception
+                  of themselves! They're slowed down by their perception of
+                  themselves. If you're taught you can’t do anything, you
+                  won’t do anything. I was taught I could do everything.
+                  I always felt like I could do anything. That’s the main
+                  thing people are controlled by! Thoughts- their perception
+                  of themselves! They're slowed down by their perception of
+                  themselves. If you're taught you can’t do anything, you
+                  won’t do anything. I was taught I could do everything.
+                  I always felt like I could do anything. That’s the main
+                  thing people are controlled by! Thoughts- their perception
+                  of themselves! They're slowed down by their perception of
+                  themselves. If you're taught you can’t do anything, you
+                  won’t do anything. I was taught I could do everything.
+                </p>
+              </div>
+              {/*footer*/}
+              <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                <button
+                  className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                  type="button"
+                  onClick={() => setShowModal(false)}
+                >
+                  Cerrar
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+      </>
+      ): null}
     </>
   );
 };
