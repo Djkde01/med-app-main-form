52a4033430658265d5dfd28ee64e5c119ba2565c
JGaviria0 <j.gaviria@utp.edu.co>
2023-03-23 09:58:56 -0500

    Fix: Modal in phone

--- pages/form.js
+++ pages/form.js
@@ -393,7 +393,7 @@ const MainForm = () => {
       {showModal? (
         <>
         <div
-          className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+          className="justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
         >
           <div className="relative w-auto my-6 mx-auto max-w-3xl">
             {/*content*/}
